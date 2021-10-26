a4a4cacf1018e7f8cc50e48f18113e6848fe0614
abbangtor <angla.tornros@abbgymnasiet.se>
2021-10-26 15:02:06 +0200

    zimon har fixat kafeterians chart och tagit bort onödig kod

--- graf.js
+++ graf.js
@@ -1,83 +1,21 @@
-let table = {
-
-}
-
-
-
-var firebase_get_value = firebase.database().ref('/')
-firebase_get_value.on('value', (snapshot) => {
-    let data = snapshot.val()
-    if (data != null) {
-        console.log(temp)
-        data = data.map(Number)
-        temp = data
-        console.log(data)
-        chart.data.datasets[0].data = temp
-        chart.update()
-        table = data
-    }
-})
-
-
-
-
-function updateChart (array) {
-    for (let i = 0; i < array.length; i++) {
-        chart.data.datasets[0].data[i] = array[0][i]
-        chart.data.datasets[1].data[i] = array[1][i]
-        chart.data.datasets[2].data[i] = array[2][i]
-    }
-}
-function getTemp () {
-    HEAD
-
-    var Lab_ettTempRef2 = firebase.database().ref('/Klassrummet/Temperature')
-    Lab_ettTempRef2.on('value', (snapshot) => {
-        let data = snapshot.val()
-        if (data != null) {
-            console.log(temp)
-            data = data.map(Number)
-            temp = data
-            console.log(temp)
-            chart.data.datasets[0].data = temp
-            chart.update()
-        }
-    })
-}
-function getHum () {
-    var Lab_ettHumRef2 = firebase.database().ref('/Klassrummet/Humidity')
-    Lab_ettHumRef2.on('value', (snapshot) => {
-        let data = snapshot.val()
-        if (data != null) {
-            console.log(hum)
-            data = data.map(Number)
-            hum = datahum
-            console.log(hum)
-            chart.data.datasets[0].data = hum
-            chart.update()
-        }
-    })
-}
-
-
-
-
-
-
-
 var xValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+var yValues = [0, 5, 10, 15, 20, 25, 30, 35, 40]
 
-let chart_kafeterian = new Chart("chart_kafeterian", {
+let chart_kafeterian = new Chart(document.getElementById('myChart'), {
     type: "line",
     data: {
         labels: xValues,
         datasets: [{
-            data: temp,
+            label: 'temp',
+            data: yValues,
             borderColor: "red",
+            backgroundColor: "red",
             fill: false
         }, {
-            data: hum,
+            label: 'hum',
+            data: [1],
             borderColor: "green",
+            backgroundColor: "green",
             fill: false
         }
 
@@ -117,54 +55,20 @@ let chart_kafeterian = new Chart("chart_kafeterian", {
     }
 })
 
-var xValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+function updateChartData (chart, data) {
 
-let cart2 = new Chart("myChart2", {
-    type: "line",
-    data: {
-        labels: xValues,
-        datasets: [{
-            data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
-            borderColor: "red",
-            fill: false
-        }, {
-            data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
-            borderColor: "green",
-            fill: false
-        }, {
-            data: [300, 700, 2000, 5000, 6000, 4000, 2000, 1000, 200, 100],
-            borderColor: "blue",
-            fill: false
-        }]
-    },
-    options: {
-        legend: { display: false }
+    for (let i = 0; i < 48; i++) {
+        chart.data.datasets[0].data[i] = Object.values(data['Temp'])[i]
+        chart.data.datasets[1].data[i] = Object.values(data['Hum'])[i]
     }
-})
 
-function getTemp () {
-
-    var terrarietTempRef2 = firebase.database().ref('/Klassrummet/Temperature')
-
-    terrarietTempRef2.on('value', (snapshot) => {
-
-        let data = snapshot.val()
-
-        if (data != null) {
-
-            console.log(temp)
-
-            data = data.map(Number)
-
-            temp = data
-
-            console.log(temp)
-
-            chart.data.datasets[0].data = temp
-
-            chart.update()
-
-        }
-    })
+    chart.update()
 }
 
+firebase.database().ref('/Cafeterian/previous').on('value', (snapshot) => {
+    let data = snapshot.val()
+    if (data != null) {
+        console.log("Update chart")
+        updateChartData(chart_kafeterian, data)
+    }
+})
