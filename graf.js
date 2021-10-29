3d72593da3b72e3043c052e57b841cc67acbdc5a
abbcecek <cecilia.ek@abbgymnasiet.se>
2021-10-29 14:52:50 +0200

    Fixat grafer med hjälp av några 3:or

--- graf.js
+++ graf.js
@@ -1,4 +1,4 @@
-var xValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+var xValues = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 var yValues = [0, 5, 10, 15, 20, 25, 30, 35, 40]
 
 
@@ -392,7 +392,7 @@ firebase.database().ref('/EntreplanPT/previous').on('value', (snapshot) => {
         // console.log("Update chart")
         updateChartData(chart_översta_våningen, data)
     }
-    
+
 })
 firebase.database().ref('/Kallaren/previous').on('value', (snapshot) => {
     let data = snapshot.val()
@@ -400,7 +400,7 @@ firebase.database().ref('/Kallaren/previous').on('value', (snapshot) => {
         // console.log("Update chart")
         updateChartData(chart_Kallaren, data)
     }
-    
+
 })
 
 
